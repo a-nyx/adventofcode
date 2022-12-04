841368d010ca8d954983d3837025386948b27b16
a-nyx <NyxAdam@protonmail.com>
2022-12-04 00:16:02 -0500

    add 04

--- 2022/04/solutionA.js
+++ 2022/04/solutionA.js
@@ -2,5 +2,26 @@ const fs = require("fs");
 const data = fs.readFileSync("./input.txt", "utf8");
 
 const lines = data.split("\n");
+let counts = 0;
 
-console.log(lines);
+for (let i = 0; i < lines.length; i++) {
+  if (lines[i] == "") continue;
+
+  const ranges = lines[i].split(",");
+  const range1 = ranges[0].split("-");
+  const range2 = ranges[1].split("-");
+
+  const start1 = parseInt(range1[0]);
+  const end1 = parseInt(range1[1]);
+  const start2 = parseInt(range2[0]);
+  const end2 = parseInt(range2[1]);
+
+  if (
+    (start1 <= start2 && end1 >= end2) ||
+    (start2 <= start1 && end2 >= end1)
+  ) {
+    counts++;
+  }
+}
+
+console.log(counts);
