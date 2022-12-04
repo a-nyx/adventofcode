3e3f1f1375c18c390a7f20c8c125c9cfea4a3e9f
a-nyx <NyxAdam@protonmail.com>
2022-12-04 00:27:10 -0500

    play 04

--- 2022/04/solutionA.js
+++ 2022/04/solutionA.js
@@ -8,13 +8,13 @@ for (let i = 0; i < lines.length; i++) {
   if (lines[i] == "") continue;
 
   const ranges = lines[i].split(",");
-  const range1 = ranges[0].split("-");
-  const range2 = ranges[1].split("-");
+  const range1 = ranges[0].split("-").map(Number);
+  const range2 = ranges[1].split("-").map(Number);
 
-  const start1 = parseInt(range1[0]);
-  const end1 = parseInt(range1[1]);
-  const start2 = parseInt(range2[0]);
-  const end2 = parseInt(range2[1]);
+  const start1 = range1[0];
+  const end1 = range1[1];
+  const start2 = range2[0];
+  const end2 = range2[1];
 
   if (
     (start1 <= start2 && end1 >= end2) ||
