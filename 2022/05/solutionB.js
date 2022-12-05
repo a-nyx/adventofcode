cb0d04f1e29dcff6ded41ae8236f1114eb337c95
a-nyx <NyxAdam@protonmail.com>
2022-12-05 00:29:54 -0500

    add 05

---
+++ 2022/05/solutionB.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const data = fs.readFileSync("./input.txt", "utf8");
+
+const lines = data.split("\n");
+
+//  why not?
+const stacks = [
+  ["R", "P", "C", "D", "B", "G"],
+  ["H", "V", "G"],
+  ["N", "S", "Q", "D", "J", "P", "M"],
+  ["P", "S", "L", "G", "D", "C", "N", "M"],
+  ["J", "B", "N", "C", "P", "F", "L", "S"],
+  ["Q", "B", "D", "Z", "V", "G", "T", "S"],
+  ["B", "Z", "M", "H", "F", "T", "Q"],
+  ["C", "M", "D", "B", "F"],
+  ["F", "C", "Q", "G"],
+];
+
+for (let i = 10; i < lines.length; i++) {
+  if (lines[i] == "") continue;
+  const numbers = [...lines[i].matchAll(/[0-9]+/g)].map((e) => Number(e[0]));
+  const move = numbers[0];
+  const from = numbers[1];
+  const to = numbers[2];
+
+  const fromLength = stacks[from - 1].length;
+  const amount = move > fromLength ? fromLength : move;
+
+  const elements = stacks[from - 1].splice(
+    stacks[from - 1].length - amount,
+    amount
+  );
+  // elements.reverse();
+  stacks[to - 1].push(...elements);
+}
+
+for (let i = 0; i < stacks.length; i++) {
+  process.stdout.write(stacks[i].pop());
+}
